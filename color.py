8f1ae28e1e1c128a8db51de54b41a357a7a23831
osoriodiego <dieferosorio@utp.edu.co>
2019-04-21 15:13:26 -0500

    color list updated

---
+++ color.py
@@ -0,0 +1,109 @@
+
+# https://www.cerotec.net/tabla-colores-web/
+
+class color(object):
+
+	#ROJOS
+	rojo_indio = 205, 92, 205
+	cordal_suave = 240, 128, 128
+	salmon = 250, 128, 114
+	salmon_oscuro = 233, 150, 122
+	salmon_suave = 255, 160, 122
+	carmesi = 220, 20, 60
+	rojo = 255, 0, 0
+	rojo_fuego = 178, 34, 34
+	rojo_oscuro = 139, 0, 0
+
+	#ROSAS
+	rosa = 255, 192, 203
+	rosa_suave = 255, 182, 193
+	rosa_calido = 255, 105, 180
+	rosa_profundo = 255, 20, 147
+	rosa_violeta = 199, 21, 133
+	rosa_pastel = 219, 112, 147
+
+	#NARANJAS
+	salmon_suave = 255, 160, 122
+	naranja_coral = 255, 127, 80
+	tomate = 255, 99, 71
+	naranja_rojizo = 255, 69, 0
+	naranja_oscuro = 255, 140, 0
+	naranja_puro = 255, 165, 0
+
+	#AMARILLOS
+	amarillo_oro = 255, 215, 0
+	amarillo = 255, 255, 0
+	amarillo_suave = 255, 255, 224
+	amarillo_limon = 255, 250, 205
+	amarillo_manzana = 250, 250, 210
+
+	#PURPURAS
+	espliego = 230, 230, 250
+	cardo = 216, 191, 216
+	ciruela = 221, 160, 221
+	violeta = 238, 130, 238
+	violeta_oscuro = 148, 0, 211
+	magenta = 255, 0, 255
+	magenta_oscuro = 139, 0, 139
+	amatista = 153, 102, 204
+	azul_violeta = 138, 43, 226
+	purpura = 128, 0, 128
+	purpura_medio = 147, 112, 219
+	orquidea = 218, 112, 214
+	orquidea_medio = 186, 85, 211
+	orquidea_oscuro = 153, 50, 204
+	indigo = 75, 0, 130
+	azul_pizarra = 106, 90, 205
+	azul_pizarra_medio = 123, 104, 238
+	azul_pizarra_oscuro = 72, 61, 139
+
+	#VERDES
+	verde_amarillo = 173, 255, 47
+	verde_cartujano = 127, 255, 0
+	verde_cesped = 124, 253, 0
+	lima = 0, 255, 0
+	verde_lima = 50, 205, 50
+	verde_palido = 152, 251, 152
+	verde_claro = 144, 238, 144
+	verde_primavera = 0, 255, 127
+	verde_primavera_medio = 0, 250, 154
+	verde_mar= 46, 139, 87
+	verde_mar_claro = 32, 178, 170
+	verde_mar_medio = 60, 179, 113
+	verde_mar_oscuro = 143, 188, 143
+	verde_bosque = 34, 139, 34
+	verde = 0, 128, 0
+	verde_oscuro = 0, 100, 0
+	amarillo_verdoso = 154, 205, 50
+	oliva = 128, 128, 0
+	verde_oliva = 107, 142, 35
+	verde_oliva_oscuro = 85, 107, 47
+	aguamarina_medio = 102, 205, 170
+	cyan_oscuro = 0, 139, 139
+	carcel = 0, 128, 128
+
+	#AZULES
+
+	#MARRONES
+
+	#BLANCOS
+	blanco = 255, 255, 255
+	blanco_nieve = 255, 250, 250
+	miel_crema = 240, 255, 240
+	menta_crema = 245, 255, 250
+	azul_celeste = 240, 255, 255
+	azul_alicia = 240, 248, 255
+	blanco_fantasma = 248, 248, 255
+	blanco_humo = 245, 245, 245
+	concha = 255, 245, 238
+	beige = 245, 245, 220
+	blanco_conrdon = 253, 245, 230
+	blanco_floral = 255, 250, 240
+	blanco_marfil = 255, 255, 240
+	marfil = 255, 255, 240
+	blanco_antiguo = 250, 235, 215
+	blanco_lino = 250, 240, 230
+	lavanda = 255, 240, 245
+	palo_de_rosa = 255, 228, 225
+
+	
